aa2d0d8fb4b2e5338932d32a5fed029b545fc92d
Cedric van Putten <github@cedric.dev>
2023-02-04 18:30:05 +0100

    chore: wow a change

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Wow, a change!
 export default function App() {
   return (
     <View style={styles.container}>
